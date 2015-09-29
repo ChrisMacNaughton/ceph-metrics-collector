f60ce91d0796a016297ab0af696591ecd922aaae
Chris Holcombe <xfactor973@gmail.com>
2015-09-29 10:17:02 -0700

    Adding more error handling

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
+import glob
 import setup
 
 setup.pre_install()
-from charmhelpers.core.hookenv import Hooks, UnregisteredHookError, log, relation_get, related_units
+from charmhelpers.core.hookenv import Hooks, UnregisteredHookError, log, relation_get, related_units, status_set
 import json
 import netifaces
 import os
@@ -16,7 +17,7 @@ hooks = Hooks()
 
 @hooks.hook('config-changed')
 def config_changed():
-    pass
+    restart()
 
 
 # Find the network interfaces to listen on based on the ceph.conf info
@@ -25,72 +26,96 @@ def find_interfaces():
     # 2. What interface is it listening on?
     interfaces_to_listen_on = []
     interfaces = netifaces.interfaces()
-    osd_dump = subprocess.check_output(['ceph', 'osd', 'dump', '--format', 'json'])
     try:
-        osd_json = json.loads(osd_dump)
-        for osd in osd_json['osds']:
-            public_addr = osd['public_addr']
-            log('OSD public addr: ' + str(public_addr))
-            # Example ipv4 output: 10.0.3.213:6800/10784
-            parts = public_addr.rstrip().split(':')
-            if len(parts) != 2:
-                log('Unable to decipher the ip address of the osd from: ' + public_addr)
-                return -1
-            addr = parts[0]
-            log('OSD addr: ' + str(addr))
-            # For each interface check to see if the IP addr matches an OSD ip addr.
-            # If it does then we'll save it so that we can listen on that addr
-            try:
-                for i in interfaces:
-                    interface_ip_info = netifaces.ifaddresses(i)[netifaces.AF_INET]
-                    log('interface ip info: ' + str(interface_ip_info))
-                    # If the OSD addr == the addr we own then add that to the listening list
-                    log('comparing {} to {}'.format(interface_ip_info[0]['addr'], addr))
-                    if len(interface_ip_info) > 0:
-                        if interface_ip_info[0]['addr'].strip() == addr.strip():
-                            interfaces_to_listen_on.append(i)
-                            log('Found a match for {} on interface {}'.format(addr, i))
-                    else:
-                        log('Skipping invalid interface {}'.format(interface_ip_info))
-            except KeyError:
-                # I don't care about interfaces that don't have IP info on them
-                pass
-        return interfaces_to_listen_on
-    except ValueError as err:
-        log('Unable to decode json from ceph.  Error is: ' + err.message)
+        status_set('maintenance', 'Gathering OSD information')
+        osd_dump = subprocess.check_output(['ceph', 'osd', 'dump', '--format', 'json'])
+        try:
+            osd_json = json.loads(osd_dump)
+            for osd in osd_json['osds']:
+                public_addr = osd['public_addr']
+                # Example ipv4 output: 10.0.3.213:6800/10784
+                parts = public_addr.rstrip().split(':')
+                if len(parts) != 2:
+                    log('Unable to decipher the ip address of the osd from: ' + public_addr)
+                    return -1
+                addr = parts[0]
+                # For each interface check to see if the IP addr matches an OSD ip addr.
+                # If it does then we'll save it so that we can listen on that addr
+                try:
+                    for i in interfaces:
+                        interface_ip_info = netifaces.ifaddresses(i)[netifaces.AF_INET]
+                        # If the OSD addr == the addr we own then add that to the listening list
+                        if len(interface_ip_info) > 0:
+                            if interface_ip_info[0]['addr'].strip() == addr.strip():
+                                interfaces_to_listen_on.append(i)
+                        else:
+                            log('Skipping invalid interface {}'.format(interface_ip_info))
+                except KeyError:
+                    # I don't care about interfaces that don't have IP info on them
+                    pass
+            return interfaces_to_listen_on
+        except ValueError as err:
+            log('Unable to decode json from ceph.  Error is: ' + err.message)
+    except subprocess.CalledProcessError:
+        # Ceph is prob not operational yet.
+        status_set('maintenance', 'Failed to gather OSD info.  Waiting on Ceph to start')
 
 
 @hooks.hook('start')
 def start():
     working_dir = os.getcwd()
-    log('working dir: ' + str(working_dir))
     listen_interfaces = find_interfaces()
     log('Found listen interfaces: ' + str(listen_interfaces))
-    proc = subprocess.Popen(["hooks/decode_ceph", "-i", "eth0"], shell=True, cwd=working_dir)
-    # Write pid to /var/run/decode_ceph
-    with open('/var/run/decode_ceph', 'w+') as pid_file:
-        pid_file.write(str(proc.pid))
+
+    if listen_interfaces is None:
+        status_set('maintenance', 'Failed to find interfaces. Waiting on Ceph to start')
+        return
+
+    status_set('maintenance', 'Starting Ceph listener')
+    for interface in listen_interfaces:
+        try:
+            process = subprocess.Popen(["hooks/decode_ceph", "-i", interface],
+                                       shell=True, cwd=working_dir)
+            # Write pid to /var/run/decode_ceph
+            try:
+                with open('/var/run/decode_ceph' + interface, 'w+') as pid_file:
+                    pid_file.write(str(process.pid))
+            except IOError as err:
+                log('Unable to write pid file for ceph interface: {}.  Error: {}'.format(
+                    interface, err.message))
+        except subprocess.CalledProcessError:
+            # todo: log levels should be an enum
+            log('Unable to find pid file for listener on interface {}'.format(interface),
+                level='INFO')
 
 
 @hooks.hook('stop')
 def stop():
-    with open('/var/run/decode_ceph', 'r') as pid_file:
-        pid = pid_file.readlines()
-        # Give a chance to stop nicely
-        pid_id = int(pid[0].strip())
-        assert isinstance(pid_id, int)
-
+    # Find all started listener processes
+    results = glob.glob('/var/run/decode_ceph*')
+    for path in results:
         try:
-            os.kill(pid_id, signal.SIGTERM)
-            time.sleep(5)
-            # It should exit quickly but if it doesn't
-            os.kill(pid_id, signal.SIGKILL)
-        except OSError as err:
-            log('Unable to find decode_ceph process: ' + err.message)
+            with open(path, 'r') as pid_file:
+                pid = pid_file.readlines()
+                # Give a chance to stop nicely
+                pid_id = int(pid[0].strip())
+                assert isinstance(pid_id, int)
+
+                try:
+                    os.kill(pid_id, signal.SIGTERM)
+                    time.sleep(5)
+                    # It should exit quickly but if it doesn't
+                    os.kill(pid_id, signal.SIGKILL)
+                except OSError as err:
+                    log('Unable to kill decode_ceph process: ' + err.message)
+        except IOError as err:
+            log('Unable to open pid file for ceph listener: {}.  Error: {}'.format(
+                path, err.message))
 
 
-def restart_collectors():
-    pass
+def restart():
+    stop()
+    start()
 
 
 @hooks.hook('elasticsearch-relation-changed')
@@ -99,7 +124,6 @@ def elasticsearch_relation_changed():
     es_host_list = []
     for member in related_units():
         es_host_list.append(relation_get('private-address {}'.format(member)))
-    restart_collectors()
 
 
 if __name__ == '__main__':
