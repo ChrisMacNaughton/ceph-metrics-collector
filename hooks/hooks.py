accbd05551cdb1dec6cdaa1df1e65081e2744923
Chris Holcombe <xfactor973@gmail.com>
2015-10-06 11:31:34 -0700

    Forgot to add the status state

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -179,6 +179,6 @@ def carbon_relation_changed():
 if __name__ == '__main__':
     try:
         hooks.execute(sys.argv)
-        status_set('', '')
+        status_set('maintenance', '')
     except UnregisteredHookError as e:
         log('Unknown hook {} - skipping.'.format(e))
